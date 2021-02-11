c6b2f6f4cd976c57abf7e32320a9a2c91a8401c4
Alois Carrera <aloiscarrera@gmail.com>
2021-02-11 12:22:51 -0500

    feat: :poop: Get courses that have a rating higher or equal than 4.5

---
+++ src/index.ts
@@ -0,0 +1,108 @@
+import fetch from "node-fetch";
+import cheerio from "cheerio";
+
+interface Post {
+  id: number;
+  date: Date;
+  date_gmt: Date;
+  guid: { rendered: string };
+  modified: Date;
+  modified_gmt: Date;
+  slug: string;
+  status: string;
+  type: string;
+  link: string;
+  title: {
+    rendered: string;
+  };
+  author: number;
+  featured_media: number;
+  comment_status: string;
+  ping_status: string;
+  sticky: boolean;
+  template: string;
+  format: string;
+  meta: number[];
+  categories: number[];
+  tags: number[];
+  language: number[];
+  dealstore: number[];
+}
+
+interface PostInfo {
+  name: Post["title"]["rendered"];
+  link: Post["link"];
+}
+
+(async function () {
+  try {
+    const data = await fetch(process.env.PAGE_ENDPOINT || "error");
+
+    const jsonData = await data.json();
+
+    const posts: PostInfo[] = jsonData.map(
+      (post: Post): PostInfo => ({
+        name: post.title.rendered,
+        link: post.link,
+      })
+    );
+
+    const udemyLinks = posts.map(async (post) => {
+      const res = await fetch(post.link);
+
+      const html = await res.text();
+      const $ = cheerio.load(html);
+
+      const udemyLink = $(".btn_offer_block.re_track_btn").attr("href");
+
+      if (!udemyLink?.includes("udemy")) return;
+
+      return {
+        udemyCourseName: post.name,
+        udemyLink,
+      };
+    });
+
+    const udemyCourses = await Promise.all(udemyLinks);
+
+    const mostValuedUdemyCourses = udemyCourses.map(async (udemyCourse) => {
+      if (udemyCourse?.udemyLink) {
+        const udemyCourseResponse = await fetch(udemyCourse.udemyLink);
+
+        const html = await udemyCourseResponse.text();
+        const $ = cheerio.load(html);
+
+        const courseRank = $(
+          '.udlite-heading-sm.star-rating--rating-number--3lVe8[data-purpose="rating-number"]'
+        )
+          .text()
+          .replace(",", ".");
+        const totalScores = $(
+          '.styles--rating-wrapper--5a0Tr[data-purpose="rating"]'
+        )
+          .clone()
+          .children()
+          .remove()
+          .end()
+          .text()
+          .match(/\d+([.,]\d+)?/)?.[0]
+          ?.replace(",", "");
+
+        if (
+          parseFloat(courseRank) >= 3 &&
+          totalScores &&
+          parseInt(totalScores) > 100
+        )
+          return udemyCourse;
+      }
+
+      return;
+    });
+
+    const result = await Promise.all(mostValuedUdemyCourses);
+
+    console.log(result.filter(Boolean));
+  } catch (error) {
+    console.log(error);
+  }
+})();
